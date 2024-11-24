c02d1c00bef8f297e063d5d7b3efc3c5c045cecd
VhBuriti <victorhugoburit@hotmail.com>
2024-11-24 10:28:43 -0300

    feat: adding reverse function inside the array class

--- DataStructures/arrays.js
+++ DataStructures/arrays.js
@@ -1,37 +1,48 @@
 class MyArray {
-    constructor() {
-        this.length = 0;
-        this.data = {};
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+  shiftItems(index) {
+    // We loop through the array and shift every item replacing the desired index
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.item[this.length - 1];
+    this.length--;
+  }
+  reverse() {
+    let newIndex = 0;
+    let reversedData = {};
+
+    for (let i = this.length - 1; i >= 0; i--) {
+        reversedData[newIndex] = this.data[i];
+        newIndex++;    
     }
 
-    get(index) {
-        return this.data[index]
-    };
-
-    push(item) {
-        this.data[this.length] = item;
-        this.length++;
-        return this.length;
-    }
-    pop() {
-        const lastItem = this.data[this.length - 1];
-        delete this.data[this.length - 1];
-        this.length--;
-        return lastItem;
-    }
-    delete(index) {
-        const item = this.data[index];
-        this.shiftItems(index);
-        return item;
-    }
-    shiftItems(index) {
-        // We loop through the array and shift every item replacing the desired index
-        for (let i = index; i < this.length - 1; i++) {
-            this.data[i] = this.data[i+1]
-        }
-        delete this.item[this.length - 1];
-        this.length--;
-    }
+    this.data = reversedData;
+  }
 }
 
 const newArray = new MyArray();
