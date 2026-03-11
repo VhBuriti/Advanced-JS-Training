121fcfbfa31eff513dbb29812172c72d2dd6dcba
VhBuriti <victorhugoburit@hotmail.com>
2026-03-11 13:54:51 -0300

    feat: adding pop method

--- DataStructures-2.0/SingleLinkedLists.js
+++ DataStructures-2.0/SingleLinkedLists.js
@@ -28,4 +28,29 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if(!this.head) {
+        return undefined
+    }
+    const current = this.head
+    const newTail = current
+    while(current.next) {
+        newTail = current
+        current = current.next
+    }
+
+    this.tail = newTail
+    this.tail.next = null;
+
+    this.length--
+
+    if(this.length === 0) {
+        this.head = null;
+        this.tail = null;
+    }
+
+    return current;
+   
+   }
 }
