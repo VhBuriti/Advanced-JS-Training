9a90b2da639ca538eccb0473df2a2a02f6047835
VhBuriti <victorhugoburiti@hotmail.com>
2026-03-11 13:38:09 -0300

    feat: creating node and list class alongisde push functionality

---
+++ DataStructures-2.0/SingleLinkedLists.js
@@ -0,0 +1,30 @@
+// piece of data - val
+// reference to the next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+    this.tail = null;
+  }
+
+  push(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+}
